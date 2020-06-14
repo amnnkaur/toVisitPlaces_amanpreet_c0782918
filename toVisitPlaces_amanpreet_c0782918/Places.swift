b3c6c1770abff298ac1f730d0c531eaabcce3919
Aman Kaur <amankaur2628@gmail.com>
2020-06-14 14:02:36 -0400

    Places model class added

---
+++ toVisitPlaces_amanpreet_c0782918/Places.swift
@@ -0,0 +1,22 @@
+//
+//  Places.swift
+//  toVisitPlaces_amanpreet_c0782918
+//
+//  Created by Aman Kaur on 2020-06-14.
+//  Copyright © 2020 Aman Kaur. All rights reserved.
+//
+
+import Foundation
+
+class Places{
+    
+    var latitude: Double
+    var longitude: Double
+    
+    init(latitude:Double , longitude: Double) {
+        self.latitude = latitude
+        self.longitude = longitude
+    }
+    
+    
+}
