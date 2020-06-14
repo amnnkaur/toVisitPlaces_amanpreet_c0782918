945ff6186d68635ae56fb972b9fda6a98986d81b
Aman Kaur <amankaur2628@gmail.com>
2020-06-14 13:47:37 -0400

    Initial Commit

---
+++ toVisitPlaces_amanpreet_c0782918/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  toVisitPlaces_amanpreet_c0782918
+//
+//  Created by Aman Kaur on 2020-06-14.
+//  Copyright © 2020 Aman Kaur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
