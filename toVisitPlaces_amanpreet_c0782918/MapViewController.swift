d9fa75c158aad3e24e3257d6c7b73d5be2d84ecd
Aman Kaur <amankaur2628@gmail.com>
2020-06-14 14:26:07 -0400

    Map View added

--- toVisitPlaces_amanpreet_c0782918/MapViewController.swift
+++ toVisitPlaces_amanpreet_c0782918/MapViewController.swift
@@ -7,14 +7,42 @@
 //
 
 import UIKit
+import MapKit
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
+
+    @IBOutlet weak var mapView: MKMapView!
+    var locationManager = CLLocationManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        mapView.isZoomEnabled = false
+        mapView.delegate = self
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
     }
 
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+               let userLocation = locations[0]
+               
+               let latitude = userLocation.coordinate.latitude
+               let longitude = userLocation.coordinate.longitude
+                
+               let latDelta: CLLocationDegrees = 0.05
+               let longDelta: CLLocationDegrees = 0.05
+                
+                // 3 - Creating the span, location coordinate and region
+               let span = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: longDelta)
+               let customLocation = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+               let region = MKCoordinateRegion(center: customLocation, span: span)
+                      
+                // 4 - assign region to map
+               mapView.setRegion(region, animated: true)
+            }
 
 }
 
