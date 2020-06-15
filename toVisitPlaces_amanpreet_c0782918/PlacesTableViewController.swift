084f4163b917ee8f78dd740a9ff9f05d2c4093c0
Aman Kaur <amankaur2628@gmail.com>
2020-06-15 12:58:47 -0400

    delete row function added in table view

--- toVisitPlaces_amanpreet_c0782918/PlacesTableViewController.swift
+++ toVisitPlaces_amanpreet_c0782918/PlacesTableViewController.swift
@@ -96,17 +96,22 @@ class PlacesTableViewController: UITableViewController {
     }
     */
 
-    /*
+    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
+            
+            self.places?.remove(at: indexPath.row)
+            self.tableView.deleteRows(at: [indexPath], with: .automatic)
+            
+            print("delete")
+            
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
+    
 
     /*
     // Override to support rearranging the table view.
